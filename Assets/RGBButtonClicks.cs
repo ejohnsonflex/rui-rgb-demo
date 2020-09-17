757c52fca8597afb039705c17098c80ecfa332fd
ejohnsonflex <60490583+ejohnsonflex@users.noreply.github.com>
2020-09-17 02:50:38 +0100

    Initial Project Commit
    UI Layout with buttons and events on button clicks

---
+++ Assets/RGBButtonClicks.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RGBButtonClicks : MonoBehaviour
+{
+    [SerializeField] Text statusText;
+  
+    public void RedButton ()
+    {
+        statusText.color = Color.red;
+        statusText.text = ("Red button click captured");
+    }
+
+    public void BlueButton()
+    {
+        statusText.color = Color.blue;
+        statusText.text = "Blue button click captured";
+    }
+
+    public void GreenButton()
+    {
+        statusText.color = Color.green;
+        statusText.text = "Green button click captured";
+    }
+
+    public void OnApplicationQuit()
+    {
+        Debug.Log("Thank you, exiting in 5 seconds...");
+        Application.Quit();
+    }
+}
